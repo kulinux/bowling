b7cdd8d2c3d403267d5a285954fb1415c221881a
Paco Benitez <paco.benitez.lazaro@gmail.com>
2020-02-09 08:19:14 +0100

    three test

--- src/main/scala/bowling/Game.scala
+++ src/main/scala/bowling/Game.scala
@@ -1,12 +1,36 @@
 package bowling
 
-import cats.data.`package`.State
+import monocle.macros.GenLens
+
+import monocle.function.all._
 
 
 case class Roll(pins: Int)
 case class Frame(roll1: Option[Roll], roll2: Option[Roll])
 case class TenthFrame(extraRolls: Seq[Roll])
 case class GameState(frames: Seq[Frame])
+
+object DataImplicits {
+    implicit class RollImpl(roll: Option[Roll]) {
+        def is10() = roll.filter(_.pins == 10).isDefined
+        def sum() = roll.map(_.pins).getOrElse(0)
+    }
+    implicit class FrameImpl(frame: Frame) {
+        def isStrike() = frame.roll1.is10()
+        def isSpare() = ( frame.roll1.sum() + frame.roll2.sum() ) == 10
+        def sum() = {
+            if( isSpare() ) 0
+            else frame.roll1.sum() + frame.roll2.sum()
+        }
+        def isFinished(): Boolean = {
+            frame match {
+                case Frame(Some(Roll(r1)), None) if r1 == 10 => true
+                case Frame(Some(_), Some(_)) => true
+                case _ => false
+            }
+        }
+    }
+}
 
 object Frame {
     def first(pins: Int) = Frame(Some(Roll(pins)), Option.empty)
@@ -16,28 +40,48 @@ object GameState {
 }
 
 trait Game {
-    def isFinished(fr: Frame): Boolean = {
-        fr match {
-            case Frame(Some(Roll(r1)), None) if r1 == 10 => true
-            case Frame(Some(_), Some(_)) => true
-            case _ => false
-        }
+    import DataImplicits._
+
+    def roll(gs: GameState, pin1: Int, pin2: Int): GameState = {
+        roll( roll(gs, pin1), pin2 )
     }
     def roll(gs: GameState, pins: Int): GameState = {
         val another : Boolean =
-            gs.frames.lastOption.map( isFinished(_) )
+            gs.frames.lastOption.map( _.isFinished() )
             .getOrElse(true)
 
+        val frames = GenLens[GameState](_.frames)
+
         if(another) {
-            GameState(gs.frames :+ Frame.first(pins))
+            frames.modify(_ :+ Frame.first(pins))(gs)
         } else {
             val last = gs.frames.last
-            GameState(gs.frames.init :+ Frame(last.roll1, Some(Roll(pins))))
+            frames.modify(fr => fr.init :+ Frame(last.roll1, Some(Roll(pins))))(gs)
         }
 
     }
 
-    def score(state: GameState): Int = 0
+
+    def score(state: GameState): Int = {
+
+        def aheadSum(head: Frame, tail: Seq[Frame]): Int = {
+            if(head.isStrike()) {
+                tail.headOption.map(next => aheadSum(next, tail.tail)).getOrElse(0) +
+                    tail.tail.headOption.map(next => aheadSum(next, tail.tail.tail)).getOrElse(0)
+            } else if(head.isSpare()) {
+                tail.headOption.map(aheadSum(_, tail.tail)).getOrElse(0) 
+            } else head.sum()
+        }
+        def score(ahead: Seq[Frame]): Int = {
+            ahead match {
+                case head :: tail => 
+                    aheadSum(head, tail) + score(tail)
+                case Nil => 0
+            }
+        }
+
+        score(state.frames)
+    }
 }
 
 object GameImpl extends Game
--- src/test/scala/bowling/BowlingSpec.scala
+++ src/test/scala/bowling/BowlingSpec.scala
@@ -4,11 +4,23 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 
 class BowlingSpec extends FlatSpec with Matchers {
-    "Bowling Zero" should "give 0 points" in {
-
-        val gs = (0 to 5).foldLeft(GameState.empty) ( (ac, b) => GameImpl.roll(ac, 0) )
-        println (gs)
+    "Bowling Zero" should "give 0 points with empty" in {
+        val gs = (1 to 20).foldLeft(GameState.empty) ( (ac, b) => GameImpl.roll(ac, 0) )
         GameImpl.score(gs) shouldBe(0)
+    }
+    "Bowling Zero" should "20 points with 1 each" in {
+        val gs = (1 to 20).foldLeft(GameState.empty) ( (ac, b) => GameImpl.roll(ac, 1) )
+        GameImpl.score(gs) shouldBe(20)
+    }
+
+    "Bowling Zero" should "sum spare" in {
+        val gs = GameState.empty
+        val first = GameImpl.roll(gs, 5, 5)
+        GameImpl.score(first) shouldBe(0)
+
+        val second = GameImpl.roll(first, 5, 2)
+        GameImpl.score(second) shouldBe(14)
+
     }
 
 }
